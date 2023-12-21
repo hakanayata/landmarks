fdb408cd35ef87027cd3ac43ce608c1e7a60b34d
Hakan <s.hakanayata@gmail.com>
2023-12-21 21:55:09 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hakan on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
