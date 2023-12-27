a071e223b3661db794bcd20314239765e67b060c
Hakan <s.hakanayata@gmail.com>
2023-12-27 18:55:53 +0100

    Drawing paths and shapes final

---
+++ Landmarks/Badges/HexagonParameters.swift
@@ -0,0 +1,58 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Hakan on 26.12.23.
+//
+
+import CoreGraphics
+
+struct HexagonParameters {
+    // Define a segment structure to hold the three points that represent one side of the hexagon
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+    
+    // Add an adjustment value for tuning
+    static let adjustment: CGFloat = 0.085
+    
+    // Create an array to hold segments
+    static let segments = [
+        // data for each side of hexagon
+        // its origin is in the upper left
+        // positive x to the right, positive y down
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+    
+}
---
+++ Landmarks/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Hakan on 27.12.23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 6))
+}
